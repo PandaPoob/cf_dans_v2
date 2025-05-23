f2f83d6180cac721a7e2e2ba8fdc121edb606253
Freja Smith <frejasmith123@gmail.com>
2025-05-23 17:24:50 +0200

    Added netlify contact form

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,23 @@
+function ContactForm() {
+  return (
+    <form name="contact" method="POST" data-netlify="true">
+      <div>
+        <label htmlFor="email">Din email </label>
+        <input type="text" name="email" />
+      </div>
+
+      <div>
+        <label htmlFor="subject">Emne </label>
+        <input type="text" name="subject" />
+      </div>
+      <div>
+        <label htmlFor="bodyText">Hvad vil du gerne spørge om?</label>
+        <textarea name="bodyText"></textarea>
+      </div>
+
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+export default ContactForm;
