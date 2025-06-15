c6b59e260283c3d64242009f6f106dba36bbda9c
Freja Smith <frejasmith123@gmail.com>
2025-06-15 13:27:58 +0200

    Finished dans page mobile

--- src/components/shared/ButtonLink.tsx
+++ src/components/shared/ButtonLink.tsx
@@ -1,8 +1,17 @@
-function ButtonLink({ label, url }: { label: string; url: string }) {
+function ButtonLink({
+  label,
+  url,
+  external,
+}: {
+  label: string;
+  url: string;
+  external?: boolean;
+}) {
   return (
     <a
       className="bg-accent text-primary flex items-center justify-center rounded-xs w-auto py-2 px-6 text-sm font-alt font-medium transition-colors duration-200 hover:bg-accent-dark"
       href={url}
+      {...(external && { target: "_blank" })}
     >
       {label}
     </a>
